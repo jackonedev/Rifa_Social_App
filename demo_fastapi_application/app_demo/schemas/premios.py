dd4541b7d4935caf96ab63f5faceaa6b2135c748
unknown <jackone.action.software@gmail.com>
2023-06-20 09:59:18 -0300

    creado schemas.premios PremioBase, PremioCreated, Premio

--- demo_fastapi_application/app_demo/schemas/premios.py
+++ demo_fastapi_application/app_demo/schemas/premios.py
@@ -1,15 +1,38 @@
 from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
+from validator import validate_url
+from pydantic import validator
 
 
-class Premio(BaseModel):
-    id: int
+class PremioBase(BaseModel):
     nombre: str
     descripcion: str
-    precio_dcto: str
-    fecha_ingreso: datetime
+    precio: int
+    descuento: int
+    cantidad: int
+
+class PremioCreate(PremioBase):
     auspiciante: Optional[str] = None
     imagen: Optional[str] = None
+    imagen_url: Optional[str] = None
+
+
+    @validator('imagen_url')
+    def validate_imagen_url(cls, value):
+        if not validate_url(value):
+            raise ValueError('Invalid imagen_url')
+        return value
+
+
+class Premio(PremioBase):
+    id: int
+    auspiciante: Optional[str] = None
+    imagen: Optional[str] = None
+    imagen_url: Optional[str] = None
+    fecha_registro: datetime
+
+    class Config:
+        orm_mode = True
 
 
