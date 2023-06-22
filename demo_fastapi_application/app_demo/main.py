596d712dca8f733431d71c7606bea912d17b05f0
unknown <jackone.action.software@gmail.com>
2023-06-22 18:03:07 -0300

    added user.router to main.app

--- demo_fastapi_application/app_demo/main.py
+++ demo_fastapi_application/app_demo/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from .database.database import engine
 from .database import models
 from fastapi.middleware.cors import CORSMiddleware
-from .api import premios, clientes, rifas
+from .api import premios, clientes, rifas, users
 #https://youtu.be/ToXOb-lpipM
 
 
@@ -23,7 +23,7 @@ app.add_middleware(
 app.include_router(premios.router)
 app.include_router(clientes.router)
 app.include_router(rifas.router)
-
+app.include_router(users.router)
 
 @app.get("/")
 def root():
