6a0b6b8eba687f499ae9aab2955354a08c212ba2
unknown <jackone.action.software@gmail.com>
2023-06-23 11:49:42 -0300

    updated annotations

--- demo_fastapi_application/app_demo/auth/oauth2.py
+++ demo_fastapi_application/app_demo/auth/oauth2.py
@@ -9,7 +9,7 @@ from ..database import models
 from sqlalchemy.orm import Session
 from ..utils.config import settings
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='v1/login')
 
 
 SECRET_KEY = settings.secret_key
@@ -33,10 +33,10 @@ def verify_access_token(token: str, credentials_exception):
     try:
 
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        id: str = payload.get("user_id")
+        id = payload.get("user_id")
         if id is None:
             raise credentials_exception
-        token_data = users.TokenData(id=str(id))
+        token_data = users.TokenData(id=id)#(7:18:00)-> we can validate more than just the user_id
     except JWTError:
         raise credentials_exception
 
@@ -47,8 +47,8 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                                           detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
 
-    token = verify_access_token(token, credentials_exception)
+    token_data = verify_access_token(token, credentials_exception)
 
-    user = db.query(models.User).filter(models.User.id == token.id).first()
+    user = db.query(models.User).filter(models.User.id == token_data.id).first()
 
     return user
