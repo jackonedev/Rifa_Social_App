e46b80490b2d15270427d65e1367736ad060fcf3
unknown <jackone.action.software@gmail.com>
2023-06-24 13:53:55 -0300

    update sorteo: it print the result of the lottery by cmd, and the response in correct format

--- demo_fastapi_application/app_demo/api/sorteos.py
+++ demo_fastapi_application/app_demo/api/sorteos.py
@@ -5,7 +5,7 @@ from datetime import datetime as dt
 import random
 
 from ..database import models
-from ..schemas.sorteos import SorteoCreate, SorteoOut, SorteoUpdate
+from ..schemas.sorteos import SorteoCreate, SorteoOut, SorteoUpdate, SorteoRealizado
 from ..schemas.users import UserOut
 from ..database.database import get_db
 from ..auth.oauth2 import get_current_user
@@ -51,7 +51,8 @@ def create_sorteo(db: Session = Depends(get_db), current_user: models.User = Dep
     
     sorteo_sc = models.Sorteo_SC(
         rifas_id=sorteo.rifas_id,
-        premios_id=sorteo.premios_id
+        premios_id=sorteo.premios_id,
+        owner=current_user
     )
 
     db.add(sorteo_sc)
@@ -61,16 +62,13 @@ def create_sorteo(db: Session = Depends(get_db), current_user: models.User = Dep
     return sorteo
 
 #create a put path operation for update a method
-@router.put("/{id}", status_code=status.HTTP_202_ACCEPTED, response_model=SorteoOut)
+@router.put("/{id}", status_code=status.HTTP_202_ACCEPTED, response_model=SorteoRealizado)
 def get_sorteo(id: int, payload:SorteoUpdate, db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
     sorteo_sc = db.query(models.Sorteo_SC).filter(models.Sorteo_SC.id == id).first()
     sorteo = db.query(models.Sorteo).filter(models.Sorteo.id == id).first()
     
     if not sorteo_sc:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Sorteo con id {id} no encontrado")
-    
-    if sorteo.user_id != current_user.id:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"No tienes permisos para editar este sorteo")
     
     if sorteo.sorteado:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Este sorteo ya fue realizado")
@@ -93,6 +91,21 @@ def get_sorteo(id: int, payload:SorteoUpdate, db: Session = Depends(get_db), cur
     sorteo_sc.ganadores = ganadores
 
     db.commit()
+
+    # lets create a query for the premios model, to get the premios nombres
+    premios = db.query(models.Premio).filter(models.Premio.id.in_(premios)).all()
+    premios = [premio.nombre for premio in premios]
+
+
+    rifas_mezcladas = rifas_mezcladas[:len(premios)]
+    rifas_mezcladas = db.query(models.Rifa).filter(models.Rifa.id.in_(rifas_mezcladas)).all()
+    rifas_mezcladas = [rifa.jugador for rifa in rifas_mezcladas]
+
+
+    # lets print the winners
+    for i, ganador in enumerate(ganadores):
+        print(f"El ganador del premio {premios[i]} es {rifas_mezcladas[i]} con la rifa {ganador[1]}")
+
     return sorteo_sc
 
 # create a get path operation for get all sorteos
