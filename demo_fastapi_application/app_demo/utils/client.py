35b417acf4f8a145a0b9961e233e8821ba8e4d7f
unknown <jackone.action.software@gmail.com>
2023-07-05 21:38:08 -0300

    updated utils.client

--- demo_fastapi_application/app_demo/utils/client.py
+++ demo_fastapi_application/app_demo/utils/client.py
@@ -9,15 +9,16 @@ def create_user(username, password, service):
     if response.status_code == 201:
         print("Usuario creado exitosamente.")
     else:
-        print("Error al crear el usuario:", response.status_code)
+        print("Error al crear el usuario:", response.status_code, response.json())
 
 def login(username, password, service):
     data = {"username": username, "password": password}  # Datos de inicio de sesión
-    response = httpx.post(service, json=data)
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}  # Encabezados de la solicitud
+    response = httpx.post(service, data=data, headers=headers)
     if response.status_code == 200:
-        token = response.json().get("token")
+        token = response.json().get("access_token")
         print("Inicio de sesión exitoso.")
         print("Token de acceso:", token)
         return token
     else:
-        print("Error al iniciar sesión:", response.status_code)
+        print("Error al iniciar sesión:", response.status_code, response.json())
